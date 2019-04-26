c1f530efd7b64f56849fa213703df2086de88279
bhgomes <bhgomes.github@gmail.com>
2019-04-26 18:04:03 -0400

    add model file

--- src/base.jl
+++ src/base.jl
@@ -1,4 +1,4 @@
-# Feynman.jl
+# Feynman/base.jl
 #
 # MIT License
 #
@@ -21,15 +21,3 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-"""
-    Feynman
-_Feynman Diagrams for Julia_
-"""
-module Feynman
-
-
-include("base.jl")
-
-
-end # module
--- src/model.jl
+++ src/model.jl
@@ -1,4 +1,4 @@
-# Feynman.jl
+# Feynman/model.jl
 #
 # MIT License
 #
@@ -21,15 +21,3 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-"""
-    Feynman
-_Feynman Diagrams for Julia_
-"""
-module Feynman
-
-
-include("base.jl")
-
-
-end # module
